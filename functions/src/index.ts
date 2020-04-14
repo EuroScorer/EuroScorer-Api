aca8d4bf005511fc0caaff299d1db58886a22746
S4cha <sachadso@gmail.com>
2020-04-14 15:57:04 +0200

    Allow cross-origin requests + clean

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,18 +1,31 @@
+import * as express from 'express'
 import * as admin from 'firebase-admin'
 import * as functions from 'firebase-functions'
+import * as cors from 'cors'
+
 
 admin.initializeApp(functions.config().firebase)
-const db = admin.firestore()
 
-export const songs = functions.https.onRequest((request, response) => {
+const db = admin.firestore()
+const app = express();
+const main = express();
+
+main.use('/v1', app);
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+
+export const api = functions.https.onRequest(main);
+
+const songsRoute = functions.https.onRequest((request, response) => {
     
     const fetchCountries = db.collection('countries').get().then((snapshot) => {
         const countries: any[] = []
         snapshot.forEach((doc) => {
             const country = {
-                "code" : doc.id,
-                "name" : doc.data().name,
-                "flag" : doc.data().flag
+                code : doc.id,
+                name : doc.data().name,
+                flag : doc.data().flag
             }
             countries.push(country)
         })
@@ -21,7 +34,6 @@ export const songs = functions.https.onRequest((request, response) => {
 
     const fetchSongs = db.collection('songs').get()
     
-
     Promise.all([fetchCountries, fetchSongs]).then((result) => {
         let countries = result[0]
         const songsSnapshot = result[1]
@@ -31,9 +43,9 @@ export const songs = functions.https.onRequest((request, response) => {
                 return c.code === doc.data().countryCode
             })
             const song = {
-                "number" : doc.data().number,
-                "title" : doc.data().title,
-                "country" : matchingCountry
+                number : doc.data().number,
+                title : doc.data().title,
+                country : matchingCountry
             }
             songsArray.push(song)
         })
@@ -43,3 +55,6 @@ export const songs = functions.https.onRequest((request, response) => {
     })
 
 })
+
+// Api
+app.get('/songs', songsRoute)
