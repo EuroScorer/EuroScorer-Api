4e0d0ebd69ebe96f32298626a85cc0aafcbbfe2a
S4cha <sachadso@gmail.com>
2020-04-15 13:51:44 +0200

    Adds POST /vote

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -58,3 +58,123 @@ const songsRoute = functions.https.onRequest((request, response) => {
 
 // Api
 app.get('/songs', songsRoute)
+
+
+// test
+app.get('/songs/:id', (request, response) => {
+
+    let songId = request.params.id
+    let songRef = db.collection('songs').doc(songId)
+    songRef.get().then(doc => {
+        if (!doc.exists) {
+            console.log('No such document!');
+            response.send('No such document!')
+        } else {
+            console.log('Document data:', doc.data());
+
+            const song = {
+                number : doc.data()?.number,
+                title : doc.data()?.title
+            }
+            
+            response.send(song)
+        }
+    })
+    .catch(err => {
+        console.log('Error getting document', err);
+        response.send('Error getting document')
+    });
+})
+
+app.post('/vote', (request, response) => {
+    let votesBody = request.body.votes
+    let authToken = request.headers.authorization
+
+    if (authToken == null) {
+        response.send("You must authenticate with a valid token")
+    }
+
+    if (votesBody == null) {
+        response.send("You must provide a body with votes")
+    }
+
+    if (votesBody.length > 20) {
+        response.send("Votes should have a maximum of 20 entries")
+    }
+
+    admin.auth().verifyIdToken(authToken!).then(function(decodedToken) {
+        let phoneNumber = decodedToken.phone_number
+        let countryCode = countryCodeFromPhoneNumber(phoneNumber)
+        let userId = decodedToken.uid
+        
+        let vote = {
+            user: userId,
+            country: countryCode,
+            votes: votesBody
+        }
+        response.send(vote)
+
+    }).catch(function(error) {
+    // Handle error
+        response.send("Error decoding token")
+    });
+
+    // TODO.
+    // Save vote to database.
+})
+
+function countryCodeFromPhoneNumber(phoneNumber: String) {
+    type myMap = {
+        [key: string]: string
+    }
+    let prefixes: myMap = {
+        "355" : "AL",
+        "374" : "AM",
+        "43" : "AT",
+        "61" : "AU",
+        "994" : "AZ",
+        "32" : "BE",
+        "359" : "BG",
+        "375" : "BY",
+        "41" : "CH",
+        "357" : "CY",
+        "420" : "CZ",
+        "49" : "DE",
+        "45" : "DK",
+        "372" : "EE",
+        "34" : "ES",
+        "358" : "FI",
+        "33" : "FR",
+        "44" : "GB",
+        "995" : "GE",
+        "30" : "GR",
+        "385" : "HR",
+        "36" : "HU",
+        "353" : "IE",
+        "972" : "IL",
+        "354" : "IS",
+        "39" : "IT",
+        "370" : "LT",
+        "371" : "LV",
+        "373" : "MD",
+        "389" : "MK",
+        "356" : "MT",
+        "31" : "NL",
+        "47" : "NO",
+        "48" : "PL",
+        "351" : "PT",
+        "40" : "RO",
+        "381" : "RS",
+        "7" : "RU",
+        "46" : "SE",
+        "386" : "SI",
+        "378" : "SM",
+        "380" : "UA"
+    }
+    let twoDigitcode = phoneNumber.substring(1, 3)
+    let threeDigitcode = phoneNumber.substring(1, 4)
+    if (prefixes[twoDigitcode] != undefined) {
+        return prefixes[twoDigitcode]
+    }
+    return prefixes[threeDigitcode]
+}
