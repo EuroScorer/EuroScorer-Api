cfb153800817f7437e0d48b8049effb83893eeb2
S4cha <sachadso@gmail.com>
2020-05-20 15:12:45 +0200

    Desactivates voting

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -67,55 +67,55 @@ app.get('/songs', songsRoute)
 
 
 app.post('/vote', (request, response) => {
-    const votesBody = request.body.votes
-    const authToken = request.headers.authorization
+    response.status(404).send("Voting ended on May 20th at 00:00")
 
-    if (authToken === undefined) {
-        response.status(401).send("You must authenticate with a valid token")
-        return
-    }
+    // const votesBody = request.body.votes
+    // const authToken = request.headers.authorization
 
-    console.log(`votesBody ${votesBody}`)
-    if (votesBody === undefined) {
-        console.log(`NO votes body in request : ${request}, body: ${request.body}`)
-        response.status(400).send("You must provide a body with votes")
-        return
-    }
+    // if (authToken === undefined) {
+    //     response.status(401).send("You must authenticate with a valid token")
+    //     return
+    // }
 
-    console.log(`votesBody.length ${votesBody.length}`)
-    if (votesBody.length > 20) {
-        response.status(400).send("Votes should have a maximum of 20 entries")
-        return
-    }
+    // console.log(`votesBody ${votesBody}`)
+    // if (votesBody === undefined) {
+    //     console.log(`NO votes body in request : ${request}, body: ${request.body}`)
+    //     response.status(400).send("You must provide a body with votes")
+    //     return
+    // }
 
-    console.log(`verifying token: ${authToken}`)
-    admin.auth().verifyIdToken(authToken).then(function (decodedToken) {
-        let phoneNumber = decodedToken.phone_number
-        let countryCode = countryCodeFromPhoneNumber(phoneNumber)
-        response.send(`countryCode ${countryCode}`)
-        
-        let userId = decodedToken.uid
+    // console.log(`votesBody.length ${votesBody.length}`)
+    // if (votesBody.length > 20) {
+    //     response.status(400).send("Votes should have a maximum of 20 entries")
+    //     return
+    // }
 
-        console.log(`Voting from phoneNumber: ${phoneNumber},  countryCode: ${countryCode}, userId ${userId}`)
-        let vote = {
-            user: userId,
-            country: countryCode,
-            votes: votesBody
-        }
+    // console.log(`verifying token: ${authToken}`)
+    // admin.auth().verifyIdToken(authToken).then(function (decodedToken) {
+    //     let phoneNumber = decodedToken.phone_number
+    //     let countryCode = countryCodeFromPhoneNumber(phoneNumber)
+    //     let userId = decodedToken.uid
 
-        // Add a new document in collection "cities" with ID 'LA'
-        db.collection('votes').doc(userId).set(vote).then((result) => {
-            console.log(`Succes saving vote`)
-            response.send(vote)
-        }).catch(function (error) {
-            console.log(`Error saving vote : ${error}`)
-            response.send("Error Saving vote")
-        })
+    //     console.log(`Voting from phoneNumber: ${phoneNumber},  countryCode: ${countryCode}, userId ${userId}`)
+    //     let vote = {
+    //         user: userId,
+    //         country: countryCode,
+    //         votes: votesBody
+    //     }
 
-    }).catch(function (error) {
-        console.log(`Error decoding token : ${error}`)
-        response.status(403).send(`Error decoding token ${error}`)
-    });
+    //     // Add a new document in collection "cities" with ID 'LA'
+    //     db.collection('votes').doc(userId).set(vote).then((result) => {
+    //         console.log(`Succes saving vote`)
+    //         response.send(vote)
+    //     }).catch(function (error) {
+    //         console.log(`Error saving vote : ${error}`)
+    //         response.send("Error Saving vote")
+    //     })
+
+    // }).catch(function (error) {
+    //     console.log(`Error decoding token : ${error}`)
+    //     response.status(403).send(`Error decoding token ${error}`)
+    // });
 })
 
 app.get('/vote', (request, response) => {
@@ -213,62 +213,62 @@ app.get('/votesCount', (request, response) => {
         })
 })
 
-function countryCodeFromPhoneNumber(phoneNumber: String) {
-    type myMap = {
-        [key: string]: string
-    }
-    let prefixes: myMap = {
-        "355": "AL",
-        "374": "AM",
-        "43": "AT",
-        "61": "AU",
-        "994": "AZ",
-        "32": "BE",
-        "359": "BG",
-        "375": "BY",
-        "41": "CH",
-        "357": "CY",
-        "420": "CZ",
-        "49": "DE",
-        "45": "DK",
-        "372": "EE",
-        "34": "ES",
-        "358": "FI",
-        "33": "FR",
-        "44": "GB",
-        "995": "GE",
-        "30": "GR",
-        "385": "HR",
-        "36": "HU",
-        "353": "IE",
-        "972": "IL",
-        "354": "IS",
-        "39": "IT",
-        "370": "LT",
-        "371": "LV",
-        "373": "MD",
-        "389": "MK",
-        "356": "MT",
-        "31": "NL",
-        "47": "NO",
-        "48": "PL",
-        "351": "PT",
-        "40": "RO",
-        "381": "RS",
-        "7": "RU",
-        "46": "SE",
-        "386": "SI",
-        "378": "SM",
-        "380": "UA"
-    }
-    let oneDigitcode = phoneNumber.substring(1, 2)
-    if (prefixes[oneDigitcode] !== undefined) {
-        return prefixes[oneDigitcode]
-    }
-    let twoDigitcode = phoneNumber.substring(1, 3)
-    if (prefixes[twoDigitcode] !== undefined) {
-        return prefixes[twoDigitcode]
-    }
-    let threeDigitcode = phoneNumber.substring(1, 4)
-    return prefixes[threeDigitcode]
-}
+// function countryCodeFromPhoneNumber(phoneNumber: String) {
+//     type myMap = {
+//         [key: string]: string
+//     }
+//     let prefixes: myMap = {
+//         "355": "AL",
+//         "374": "AM",
+//         "43": "AT",
+//         "61": "AU",
+//         "994": "AZ",
+//         "32": "BE",
+//         "359": "BG",
+//         "375": "BY",
+//         "41": "CH",
+//         "357": "CY",
+//         "420": "CZ",
+//         "49": "DE",
+//         "45": "DK",
+//         "372": "EE",
+//         "34": "ES",
+//         "358": "FI",
+//         "33": "FR",
+//         "44": "GB",
+//         "995": "GE",
+//         "30": "GR",
+//         "385": "HR",
+//         "36": "HU",
+//         "353": "IE",
+//         "972": "IL",
+//         "354": "IS",
+//         "39": "IT",
+//         "370": "LT",
+//         "371": "LV",
+//         "373": "MD",
+//         "389": "MK",
+//         "356": "MT",
+//         "31": "NL",
+//         "47": "NO",
+//         "48": "PL",
+//         "351": "PT",
+//         "40": "RO",
+//         "381": "RS",
+//         "7": "RU",
+//         "46": "SE",
+//         "386": "SI",
+//         "378": "SM",
+//         "380": "UA"
+//     }
+//     let oneDigitcode = phoneNumber.substring(1, 2)
+//     if (prefixes[oneDigitcode] !== undefined) {
+//         return prefixes[oneDigitcode]
+//     }
+//     let twoDigitcode = phoneNumber.substring(1, 3)
+//     if (prefixes[twoDigitcode] !== undefined) {
+//         return prefixes[twoDigitcode]
+//     }
+//     let threeDigitcode = phoneNumber.substring(1, 4)
+//     return prefixes[threeDigitcode]
+// }
